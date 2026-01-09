968d4c42c27431cf7b649aae4fa2b2b5ae212f0f
reagan luyinda <luyindareagan@gmail.com>
2026-01-09 18:32:45 +0300

    create app in the root defining actual express application

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const blogsRouter = require("./controllers/blogs");
+const morgan = require("morgan");
+
+const app = express();
+logger.info("Connecting to MongoDB:", config.mongoUrl);
+
+mongoose
+  .connect(config.mongoUrl, { family: 4 })
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.info("Error connecting to MongoDB:", error.message);
+  });
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(middleware.requestLogger);
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
