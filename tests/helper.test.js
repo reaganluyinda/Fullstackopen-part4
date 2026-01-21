ddcd631b79ba33b42830d4c22f18d2f26a7b2300
reagan luyinda <luyindareagan@gmail.com>
2026-01-21 13:31:52 +0300

    4.4 helper func & unit test, step 2; write tests for total likes

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -2,11 +2,41 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-describe('helper', () => {
-  test('dummy return one', () => {
-    const blogs = []
+test('dummy return one', () => {
+  const blogs = []
 
-    const result = listHelper.dummy(blogs)
-    assert.strictEqual(result, 1)
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+
+  ]
+
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [{ likes:5 }, { likes:10 }, { likes:2 }]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 17)
   })
 })
