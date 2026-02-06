595f84fbb4f236a6f2d136d0a63d54df3f637c7a
reagan luyinda <luyindareagan@gmail.com>
2026-02-06 12:41:35 +0300

    4.10 Blog Lists Tests, step 3; create test for blog post, verify total number of blogs in the db if they have increased by one and saved correctly, refactor blog post route handler

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,12 +29,43 @@ after(async () => {
   await mongoose.connection.close
 })
 
-test.only('blogs have an id property', async () => {
+//test for id property
+test('blogs have an id property', async () => {
   const response = await api.get('/api/blogs')
   const blog = response.body[0]
 
   assert(blog.id)
   assert.strictEqual(blog._id, undefined)
   console.log(response.body)
+
+})
+
+//test for adding a new blog
+test.only('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Reagan Beckams',
+    url: 'http://www.testblog.com',
+    likes: 10,
+  }
+
+  const blogsAtStart = await helper.blogInDb()
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogInDb()
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+
+
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(blog => blog.title)
+
+  assert(titles.includes('Test Blog'))
+  //   console.log(response.body)
 
 })
