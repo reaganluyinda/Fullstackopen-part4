4366ef4b7d16127f70e6955122d1e3de676e7d2b
reagan luyinda <luyindareagan@gmail.com>
2026-02-22 00:22:40 +0300

    4.6: Blog List Expansion, step 4; create user model, create users router and import it to be used by the app and use rest client to test apis un all_routes.rest

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const morgan = require('morgan')
 
 const app = express()
@@ -21,6 +22,8 @@ mongoose
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
