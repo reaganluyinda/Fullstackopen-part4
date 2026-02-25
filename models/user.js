d38c21f62c1e6cd6d7eb9a2666e762d448f11cc8
reagan luyinda <luyindareagan@gmail.com>
2026-02-25 14:15:20 +0300

    referencing the Blog and User models

--- models/user.js
+++ models/user.js
@@ -4,6 +4,12 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true, minlength: 3 },
   name: String,
   passwordHash: { type: String, required: true },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
 })
 
 userSchema.set('toJSON', {
