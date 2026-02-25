a6b7339b2b7dce164414d9c0fec816854ae8c48c
reagan luyinda <luyindareagan@gmail.com>
2026-02-25 14:17:22 +0300

    4.17: Blog List Expansion, step5; add user information to the blogs and blogs created by each user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 //route for fetching all blogs
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -15,16 +16,20 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'title and url are required' })
   }
 
+  const users = await User.find({})
+  const user = users[2]
+
   const blog = new Blog({
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes || 0,
+    user: user._id
   })
 
-
-
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
