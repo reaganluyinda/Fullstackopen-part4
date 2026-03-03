525d9d07292e9b4b6a66164b2082650851f10d7e
reagan luyinda <luyindareagan@gmail.com>
2026-03-03 16:29:31 +0300

    4.23 Blog List Expansion, step 11; fix add blog test and delete blog, write unathorized blog delete and test for proper status code for unathorized add blog

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,15 +3,33 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
 const { test, after, beforeEach, describe } = require('node:test')
+const bcrypt = require('bcrypt')
+
 
 
 const api = supertest(app)
 
+let token
+
 beforeEach(async () => {
   await Blog.deleteMany({})
+  await User.deleteMany({})
+
   await Blog.insertMany(helper.initialBlogs)
+
+  const passwordHash = await bcrypt.hash('maguire123', 10)
+  const user = new User({ username: 'maguire', passwordHash })
+  await user.save()
+
+  const result = await api
+    .post('/api/login')
+    .send({ username: 'maguire', password: 'maguire123' })
+
+  token = result.body.token
+
 })
 
 //test for get and json
@@ -39,6 +57,7 @@ test('blogs have an id property', async () => {
 
 //test for adding a new blog
 test('a valid blog can be added', async () => {
+
   const newBlog = {
     title: 'Test Blog',
     author: 'Reagan Beckams',
@@ -50,6 +69,7 @@ test('a valid blog can be added', async () => {
 
   await api
     .post('/api/blogs')
+    .set('Authorization', `Bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
@@ -67,6 +87,27 @@ test('a valid blog can be added', async () => {
 
 })
 
+//test for adding a blog without token fails
+test('a blog cannot be added without token', async () => {
+  const newBlog = {
+    title: 'Test Blog Without Token',
+    author: 'Reagan Beckams',
+    url: 'http://www.testblogwithouttoken.com',
+    likes: 10,
+  }
+
+  const blogsAtStart = await helper.blogInDb()
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(401)
+
+  const blogsAtEnd = await helper.blogInDb()
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length)
+
+})
+
 //test blog missing likes
 test('if likes property is missing, it defaults to 0', async () => {
   const newBlog = {
@@ -77,6 +118,7 @@ test('if likes property is missing, it defaults to 0', async () => {
 
   const response = await api
     .post('/api/blogs')
+    .set('Authorization', `Bearer ${token}`)
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
@@ -99,6 +141,7 @@ describe('test a blog without title and url', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(400)
 
@@ -121,6 +164,7 @@ describe('test a blog without title and url', () => {
 
     await api
       .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
       .send(newBlog)
       .expect(400)
 
@@ -134,22 +178,78 @@ describe('test a blog without title and url', () => {
 
 describe('test deleting a blog', () => {
   test('a blog can be deleted', async () => {
+
     const blogsAtStart = await Blog.find({})
-    const blogToDelete = blogsAtStart[0]
+    // const blogToDelete = blogsAtStart[0]
+
+    const newBlog = {
+      title: 'Blog to be deleted',
+      author: 'tester',
+      url: 'http://www.blogtobedeleted.com',
+      likes: 5,
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogToDelete = response.body
 
     await api
       .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(204)
 
     const blogsAtEnd = await Blog.find({})
 
-    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length)
 
     const titles = blogsAtEnd.map(blog => blog.title)
 
     assert(!titles.includes(blogToDelete.title))
   })
 
+  test('a blog cannot be deleted by another user', async () => {
+
+    const blogsAtStart = await Blog.find({})
+
+    const newBlog = {
+      title: 'Blog to be deleted',
+      author: 'tester',
+      url: 'http://www.blogtobedeleted.com',
+      likes: 5,
+    }
+
+    const response = await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogToDelete = response.body
+
+    const passwordHash = await bcrypt.hash('user123', 10)
+    const otherUser = new User({ username: 'user2', passwordHash })
+    await otherUser.save()
+
+    const user2LoginResponse = await api
+      .post('/api/login')
+      .send({ username: 'user2', password: 'user123' })
+
+    const otherUserToken = user2LoginResponse.body.token
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `Bearer ${otherUserToken}`)
+      .expect(403)
+
+    const blogsAtEnd = await Blog.find({})
+
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+  })
 })
 
 test('a blog can be updated', async () => {
